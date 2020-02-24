61e3ae749171281a80376983aba6d00674d9aad1
MadhaviGunapati610 <59458995+MadhaviGunapati610@users.noreply.github.com>
2020-02-24 17:00:20 -0500

    adding more logic

---
+++ src/components/Game/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Board from '../Board';
+
+class Game extends React.Component{
+  constructor(props){
+super(props);
+this.state={
+xIsNext : true,
+stepNumber: 0,
+history: [
+{square: Array(9).fill(null)}
+]
+}
+  }
+render(){
+const history = this.state.history;
+const current= history[this.state.stepNumber]
+  return(
+<div className="game">
+  <div className="board-game">
+<Board onClick={(i)=>this.onClick(i)}
+squares={current.squares}
+/>
+  </div>
+</div>
+  )
+}
+}
+
+export default Game
